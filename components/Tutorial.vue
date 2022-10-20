26301f45da2afafd4e9db18b61d02b2db2e56f6b
Talha Can Havadar <talha.can.havadar@canonical.com>
2022-10-20 23:46:27 +0000

    Create initial files with linting configurations

---
+++ components/Tutorial.vue
@@ -0,0 +1,10 @@
+<!-- Please remove this file from your project -->
+<template>
+    <h1 class="bg-sky-700 text-right text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<script>
+export default {
+    name: 'NuxtTutorial',
+}
+</script>
